cc1033f57fb088b1e3fa6dc740f50e8bfdd79896
nlthanhcse <nlthanhititiu17038@gmail.com>
2020-07-29 11:26:16 +0700

    test

--- user-comment-service/src/main/java/ojt/thanhnl4/usercommentservice/UserCommentServiceApplication.java
+++ user-comment-service/src/main/java/ojt/thanhnl4/usercommentservice/UserCommentServiceApplication.java
@@ -2,8 +2,10 @@ package ojt.thanhnl4.usercommentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class UserCommentServiceApplication {
 
 	public static void main(String[] args) {
