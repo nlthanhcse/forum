06bcf51ee2e44711eaf083558f8c4580ccc51678
nlthanhcse <nlthanhititiu17038@gmail.com>
2020-07-29 10:22:53 +0700

    config eureka client and register user-post-service to forum-server

---
+++ category-post-service/src/main/java/ojt/thanhnl4/categorypostservice/model/CategoryPost.java
@@ -0,0 +1,19 @@
+package ojt.thanhnl4.categorypostservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "category_post")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CategoryPost {
+
+}
--- user-post-service/src/main/java/ojt/thanhnl4/userpostservice/UserPostServiceApplication.java
+++ user-post-service/src/main/java/ojt/thanhnl4/userpostservice/UserPostServiceApplication.java
@@ -2,8 +2,10 @@ package ojt.thanhnl4.userpostservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class UserPostServiceApplication {
 
 	public static void main(String[] args) {
