85439e8607aebc48384d5a8ad3f5b33dcba72e25
nlthanhcse <nlthanhititiu17038@gmail.com>
2020-07-31 13:06:05 +0700

    in progress

--- forum-api-gateway/src/main/java/ojt/thanhnl4/forumapigateway/ForumApiGatewayApplication.java
+++ forum-api-gateway/src/main/java/ojt/thanhnl4/forumapigateway/ForumApiGatewayApplication.java
@@ -6,6 +6,8 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
